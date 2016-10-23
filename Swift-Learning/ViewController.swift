85feb06aef5b3b0fbf62bb8ddeeb7639b7ce8809
Artem Kutukov <artem.kutukov@gentechpartners.com>
2016-10-23 23:59:24 +0300

    add new func Planet

--- Swift-Learning/ViewController.swift
+++ Swift-Learning/ViewController.swift
@@ -14,15 +14,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let incrementByTen = makeIncrementer(forIncrement: 10)
+        print(incrementByTen())//10
+        print(incrementByTen())//20
+        
+        var directionToHead = Planet.earth
+    }
+
+    func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+        var runningTotal = 0
+        func incrementer() -> Int {
+            runningTotal += amount
+            return runningTotal
+        }
+        return incrementer
     }
     
-    func factorial(number: Int) -> (Int) {
-        if (number <= 1) {
-            return 1
+    
+    func getWeightPlanet(planet: Planet) -> Double {
+        if planet == Planet.earth {
+            return  pow(5.972, 24)
+        } else if  planet == Planet.mars{
+            return  pow(6.39, 23)
+        } else if  planet == Planet.jupiter{
+            return  pow(1.898, 27)
         }
-
-        
-        return number * factorial(number: number - 1)
+        return 0
+    }
+    
+    enum Planet {
+        case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
     }
     
 }
